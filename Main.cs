32c41be6d566948d8a92eadd6e83df17e20888eb
NikitaKop <60967083+NikitaKop@users.noreply.github.com>
2020-03-19 13:27:24 +0300

    Commit 19.0.2020

--- Main.cs
+++ Main.cs
@@ -6,7 +6,9 @@ namespace Lab
     {
         public static void Main(string[] args)
         {
-            int i = 2+2;
+            int i = 2 + 2;
+            i += 4;
+            //I wish author to end 3rd class! ;)
         }
     }
 }
